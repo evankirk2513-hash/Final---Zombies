d27d382beb368a90f9650a8bb025e11024a9d8d5
evan.kirk2513-hash <evan.kirk2513@gmail.com>
2026-05-08 16:24:34 -0500

    Finished player and bullet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import *
 from random import randint, choice
 
+def generate_color():
+    return f"#{randint(0, 0xFFFFFF):06x}"
+
 #### CLASS AND FUNCTION DEFINITIONS #####
 def playing_area():
 	t = Turtle()
@@ -16,68 +19,85 @@ def playing_area():
 		t.right(90)
 	t.end_fill()
 
-'''
-Player() Class
-
-Constructor( def __init__(self)):
-- player should be shaped like a turtle.
-- will take in the x and y coordinates for where the player will initially appear.
-- will take in a color for the player
-- will take in keys to turn left, turn right and shoot bullets.
-- player will have an attribute that is a list that stores bullets
-
-
-move(self):
-- moves object forward five pixels
-
-fire(self):
-- creates a Bullet object
-- appends the Bullet object to the players's bullet list
-'''
 class Player(Turtle):
-	def __init__(self,screen,left_key,right_key,fire_key,x,y):
-		self.speed(0)
+	def __init__(self,screen,left_key,right_key,fire_key,bomb_key):
+		super().__init__()
 		self.ht()
+		self.speed(0)
 		self.penup()
-		self.goto(x,y)
+		self.color(generate_color())
+		self.goto(randint(-230,230),randint(-230,230))
 		self.shape("turtle")
+		self.bombs = 3
 		self.bullets = []
-		screen.onkeypress(self.rt(),right_key)
-		screen.onkeypress(self.lt(),left_key)
-		screen.onkeypress(self.fire(),fire_key)
+		self.alive = True
+		self.st()
+		screen.onkeypress(self.turn_right,right_key)
+		screen.onkeypress(self.turn_left,left_key)
+		screen.onkeypress(self.fire,fire_key)
+		screen.onkey(self.bomb,bomb_key)
 	
-	def rt(self):
-		self.rt(2)
+	def turn_right(self):
+		self.rt(10)
 
-	def lt(self):
-		self.lt(2)
+	def turn_left(self):
+		self.lt(10)
 
 	def move(self):
 		self.forward(5)
+		if self.xcor() > 230 or self.xcor() < -230:
+			self.setheading(180 - self.heading())
+		if self.ycor() > 230 or self.ycor() < -230:
+			self.setheading(-self.heading())
 
 	def fire(self):
-		pass
+		self.bullets.append(Bullet(self))
+	
+	def bomb(self):
+		self.bombs -= 1
+		bomb = Turtle()
+		bomb.ht()
+		bomb.speed(0)
+		bomb.penup()
+		bomb.goto(self.xcor(),self.ycor())
+		bomb.color(self.player_color)
+		bomb.shape("circle")
+		bomb.shapesize(5,5)
 
+class Bullet(Turtle):
+    def __init__(self, player):
+        super().__init__()
+        self.ht()
+        self.speed(0)
+        self.color(player.player_color)
+        self.penup()
+        self.setheading(player.heading()) 
+        self.goto(player.xcor(),player.ycor())
+        self.forward(10)
+        self.player = player
+        self.st()
+    
+    def move(self):
+        self.forward(15)
+        if self.xcor() > 230 or self.xcor() < -230:
+            self.remove()
+        if self.ycor() > 230 or self.ycor() < -230:
+            self.remove()
+    
+    def remove(self):
+        if self in self.player.bullets:
+            self.ht()
+            self.player.bullets.remove(self)
 
-
-'''
-Bullet() Class
-Constructor ( def __init__(self) ):
-- Input: player object
-- Attributes:
-	- Position: same as player
-	- Heading: same as player
-	- Player: the player
- 
-move(self):
-- move 15 or more pixels forward
-- should call on the die() method when the bullet leaves the playing area
-
-die()
-- hides the object. 
-- removes object from the player's bullet list
-'''
-
+class Zombie(Turtle):
+	def __init__(self,target):
+		super().__init__()
+		self.ht()
+		self.speed(0)
+		self.penup()
+		self.color("green")
+		self.shape("turtle")
+		self.heading(self.towards(target))
 
 #### DRIVER CODE ####
 screen = Screen()
@@ -86,9 +106,13 @@ screen.listen()
 
 playing_area()
 
-player1 = Player(screen,"Left","Right","Up",10,10)
+player1 = Player(screen,"Left","Right","Up","Down")
 
 while True:
 	pass
 
 screen.mainloop()
+
+
+
+screen.exitonclick()
